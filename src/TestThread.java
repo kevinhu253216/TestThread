39f9e27753d2d7c68cf6d7f6866e1c8d8fd0b465
Binary&&Entropy <kevinhu253216@gmail.com>
2018-07-04 14:57:59 -0400

    Update TestThread.java

--- src/TestThread.java
+++ src/TestThread.java
@@ -4,6 +4,7 @@ public class TestThread {
         Runner1 r = new Runner1();
         Thread t = new Thread(r);
         t.start();
+        // r.run(); 不能使用，因为这是方法调用
         for(int i=0;i<100;i++){
             System.out.println("Main Thread:-----"+ i );
         }
@@ -11,7 +12,7 @@ public class TestThread {
     }
 }
 
-class Runner1 implements Runnable{
+class Runner1 implements Runnable{ //尽量使用 继承接口， 不要使用extends Thread，因为继承父类，只能实现一次，而接口可以实现多个
     public void run(){
         for(int i=0;i<100;i++){
             System.out.println("Runner1:"+i);
